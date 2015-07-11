466e37afcc99e212cc95836ab73cb51cf77cfef1
Manas Bajaj <manas.bajaj94@gmail.com>
2015-07-11 17:55:42 +0530

    Added ability to cache subreddits.

--- app/public/javascripts/collections/subreddits.js
+++ app/public/javascripts/collections/subreddits.js
@@ -12,6 +12,7 @@ define([
         initialize: function (options) {
             this.type = options.type;
         },
+        cacheTimeToLive: 86400000,
         model: SubredditModel,
         urls: {
             defaults: "/api/subreddits/defaults",
@@ -21,28 +22,45 @@ define([
             // if user is not logged in, reset collection without content
             if (this.type == "user" && !localStorage.getItem("username")) {
                 this.reset();
-                console.log("NO USER LOL");
             } else {
-                var self = this;
-                $.ajax({
-                    url: self.type == "user" ? self.urls[this.type] + "?session=" + localStorage.getItem("session") : self.urls[this.type],
-                    method: "GET",
-                    dataType: "json",
-                    timeout: 6000,
-                    success: function (subreddits) {
-                        self.reset(subreddits);
-                    },
-                    error: function (jqXHR, textStatus) {
-                        if (textStatus == "timeout") {
-                            setTimeout(function () {
-                                self.fetch();
-                            }, 1000);
-                        } else {
-                            console.log(textStatus);
-                        }
+                var cachedSubreddits = localStorage.getItem(this.type + "_subreddits");
+                if (cachedSubreddits) {
+                    cachedSubreddits = JSON.parse(cachedSubreddits);
+                    // use cached subreddits if they are not older than 1 day
+                    if ((Date.now() - cachedSubreddits.timestamp) <= this.cacheTimeToLive) {
+                        this.reset(cachedSubreddits.subreddits);
+                    } else {
+                        this.fetchAjax();
                     }
-                });
+                } else {
+                    this.fetchAjax();
+                }
             }
+        },
+        fetchAjax: function () {
+            var self = this;
+            $.ajax({
+                url: self.type == "user" ? self.urls[this.type] + "?session=" + localStorage.getItem("session") : self.urls[this.type],
+                method: "GET",
+                dataType: "json",
+                timeout: 6000,
+                success: function (subreddits) {
+                    self.reset(subreddits);
+
+                    // cache subreddits along with a timestamp
+                    var subredditsToCache = {timestamp: Date.now(), subreddits: subreddits};
+                    localStorage.setItem(self.type + "_subreddits", JSON.stringify(subredditsToCache));
+                },
+                error: function (jqXHR, textStatus) {
+                    if (textStatus == "timeout") {
+                        setTimeout(function () {
+                            self.fetch();
+                        }, 1000);
+                    } else {
+                        console.log(textStatus);
+                    }
+                }
+            });
         }
     });
 
