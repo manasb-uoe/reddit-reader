79b8cf1d5031c6235f1804a08385f1c67b654123
Manas Bajaj <manas.bajaj94@gmail.com>
2015-09-10 00:37:25 +0530

    [frontend_android] Janitorial work: removed log statements.

--- frontend_android/app/src/main/java/com/enthusiast94/reddit_reader/app/fragments/ContentViewerFragment.java
+++ frontend_android/app/src/main/java/com/enthusiast94/reddit_reader/app/fragments/ContentViewerFragment.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -44,12 +43,6 @@ public class ContentViewerFragment extends Fragment implements OnBackPressedList
         contentViewerFragment.setArguments(bundle);
 
         return contentViewerFragment;
-    }
-
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        Log.d(TAG, "onCreate fragment");
     }
 
     @Nullable
@@ -159,7 +152,7 @@ public class ContentViewerFragment extends Fragment implements OnBackPressedList
     @Override
     public void onHiddenChanged(boolean hidden) {
         super.onHiddenChanged(hidden);
-        Log.d(TAG, hidden + ": hidden");
+
         isVisible = !hidden;
     }
 
